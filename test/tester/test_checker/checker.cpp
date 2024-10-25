1f1373b629743c41cc0bbe7462c1b832947c5b86
JamahaW <153020210+JamahaW@users.noreply.github.com>
2024-10-25 23:04:59 +0300

    motor test

--- test/tester/test_checker/checker.cpp
+++ test/tester/test_checker/checker.cpp
@@ -1,11 +1,35 @@
 #include <Arduino.h>
 
 
+#include "hardware/MotorDriver.hpp"
+#include "constants/Pins.hpp"
+
+
+auto driver = hardware::MotorDriverL293(constants::MOTOR_LEFT_DRIVER_A, constants::MOTOR_LEFT_DRIVER_B);
+
 void setup() {
+
+
     Serial.begin(9600);
-    Serial.println("Hello world!");
+    Serial.setTimeout(100);
 }
 
 void loop() {
+    if (Serial.available() == 0) {
+        return;
+    }
 
+    auto code = char(Serial.read());
+    int value = Serial.parseInt();
+
+    switch (code) {
+        case '+':driver.set(value);
+            break;
+
+        case '-':driver.set(-value);
+            break;
+
+        case 'f':analogWriteFrequency(value);
+            break;
+    }
 }
